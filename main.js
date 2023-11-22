fac250abcaab044a9205da89e3487360f310d982
MariaJulia044 <130760638+MariaJulia044@users.noreply.github.com>
2023-11-22 11:53:54 -0300

    jhgj

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
     }
+
+function tocaSomclap (){
+    document.querySelector('#som_tecla_clap').play();
+    }
+
+
     document.querySelector('.tecla_pom') .onclick = tocaSomPom;
+    document.querySelector('.tecla_clap') .onclick = tocaSomclap;
